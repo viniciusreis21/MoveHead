8d4f8f84a41cede0fa382e513092165d64a93bbc
viniciusreis21 <vinicius.reis@engenharia.ufjf.br>
2020-06-23 12:14:50 -0300

    Project MoveHead

---
+++ build-rinobot-robot/CMakeFiles/movehead.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/viniciusreis/Downloads/MoveHead/MoveHead/movehead.cpp" "/home/viniciusreis/Downloads/MoveHead/MoveHead/build-rinobot-robot/CMakeFiles/movehead.dir/movehead.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/viniciusreis/Downloads/Mari-Assets/sdk/ctc-linux64-atom-2.1.4.13/libnaoqi/include"
+  "/home/viniciusreis/Downloads/Mari-Assets/sdk/ctc-linux64-atom-2.1.4.13/boost/include/boost-1_55"
+  "/home/viniciusreis/Downloads/Mari-Assets/sdk/ctc-linux64-atom-2.1.4.13/cross/i686-aldebaran-linux-gnu/sysroot/usr/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
